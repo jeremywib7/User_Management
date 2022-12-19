83769cae5e25ccadf1a437d4527c06c1ad7a01f3
jeremyyonathan <jeremywib7@gmail.com>
2022-12-19 16:46:41 +0700

    remove unused pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
-import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { CustomerService } from './demo/service/customer.service';
-import { EventService } from './demo/service/event.service';
-import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
-import { PhotoService } from './demo/service/photo.service';
+import { NotfoundComponent } from './main/components/notfound/notfound.component';
+import { ProductService } from './main/service/product.service';
+import { CountryService } from './main/service/country.service';
+import { CustomerService } from './main/service/customer.service';
+import { EventService } from './main/service/event.service';
+import { IconService } from './main/service/icon.service';
+import { NodeService } from './main/service/node.service';
+import { PhotoService } from './main/service/photo.service';
 
 @NgModule({
     declarations: [
